120e5d40894990d2301b538b304e3a29733bff3f
mhertz <Matthew.Hertz@man.com>
2023-02-26 19:39:16 +0000

    Fix test scripts

--- scripts/test_runs/run_cpptest.sh
+++ scripts/test_runs/run_cpptest.sh
@@ -4,6 +4,6 @@ set -eux
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:/opt/man/releases/python-medusa/36-1/lib:/default-pegasus-venv/lib
 
-catchsegv cpp/tests/test_unit_arcticcxx --gtest_output=xml:gtest-cpp.xml --gtest_filter=-Server.*
+catchsegv cpp/tests/test_unit_arcticdb --gtest_output=xml:gtest-cpp.xml --gtest_filter=-Server.*
 
 python3 scripts/test_runs/gtesttojunit.py  "suitename"  "cppunit"  gtest-cpp.xml  junit-cpp.xml
--- scripts/test_runs/run_rapidcheck.sh
+++ scripts/test_runs/run_rapidcheck.sh
@@ -4,6 +4,6 @@ set -eux
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:/opt/man/releases/python-medusa/36-1/lib:/default-pegasus-venv/lib
 
-catchsegv cpp/tests/arcticcxx_rapidcheck_tests --gtest_output=xml:gtest-rapidcheck.xml
+catchsegv cpp/tests/arcticdb_rapidcheck_tests --gtest_output=xml:gtest-rapidcheck.xml
 
 python3 scripts/test_runs/gtesttojunit.py  "suitename"  "cpprapidcheck"  gtest-rapidcheck.xml  junit-rapidcheck.xml
