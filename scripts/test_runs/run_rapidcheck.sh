b46b860700346502f3cec58bc7eca51b3c23e3e9
mhertz <Matthew.Hertz@man.com>
2023-02-26 18:29:47 +0000

    Fix cpp test paths

--- scripts/test_runs/run_rapidcheck.sh
+++ scripts/test_runs/run_rapidcheck.sh
@@ -4,6 +4,6 @@ set -eux
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:/opt/man/releases/python-medusa/36-1/lib:/default-pegasus-venv/lib
 
-catchsegv tests/cpp/arcticcxx_rapidcheck_tests --gtest_output=xml:gtest-rapidcheck.xml
+catchsegv cpp/tests/arcticcxx_rapidcheck_tests --gtest_output=xml:gtest-rapidcheck.xml
 
 python3 scripts/test_runs/gtesttojunit.py  "suitename"  "cpprapidcheck"  gtest-rapidcheck.xml  junit-rapidcheck.xml
