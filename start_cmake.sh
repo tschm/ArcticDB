d47a63bc8bba75c9be85bc11f46a3cfd449541a2
mhertz <Matthew.Hertz@man.com>
2023-03-26 12:17:43 +0100

    Remove files that arent needed

--- start_cmake.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source scl_source enable devtoolset-10
-if [[ $@ == *"Unix Makefiles"* ]]; then
-  /opt/arcticc/venv/bin/cmake -DPython_ROOT_DIR="/usr/lib64" -DPython_EXECUTABLE="/usr/bin/python3" "$@"
-else
-  python -mgrpc_tools.protoc -Iarcticdb_link/cpp/proto --python_out=arcticdb_link/python/ arcticdb_link/cpp/proto/arcticc/pb2/*.proto
-  /opt/arcticc/venv/bin/cmake "$@"
-fi
