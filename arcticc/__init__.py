92a128ec946ae682de03f586b4642c7faa2ef427
Alex Seaton <alex.seaton@man.com>
2023-05-05 17:22:17 +0100

    WIP back to the old fashioned way, with fix for globals

--- arcticc/__init__.py
+++ arcticc/__init__.py
@@ -4,9 +4,7 @@ from arcticdb import *
 
 import sys
 import importlib
-import importlib.machinery
-import importlib.abc
-import importlib.util
+from dataclasses import dataclass
 
 
 # Patch imports containing "old-style" references to arcticc to the Github repo ArcticDB
@@ -14,61 +12,37 @@ import importlib.util
 # arcticc -> arcticdb
 
 
-class PatcherLoader(importlib.abc.Loader):
+@dataclass
+class PatcherLoader:
+    old_path: str
+    new_path: str
 
-    def __init__(self, old_path: str, new_path: str):
-        self.old_path = old_path
-        self.new_path = new_path
-
-    def create_module(self, spec):
+    def load_module(self, fullname):
         try:
-            return sys.modules[spec.name]
+            return sys.modules[fullname]
         except KeyError:
             pass
-        m = self._patched_module(spec.name)
-        sys.modules[spec.name] = m
-
-        # # ensure single copy of sub-modules
-        # current_modules = dict(sys.modules)
-        # for k, v in current_modules.items():
-        #     patched_name = k.replace(m.__name__, spec.name, 1)  # arcticdb.blah -> arcticc.blah
-        #     if m.__name__ not in k or patched_name in current_modules:
-        #         continue
-        #     sys.modules[patched_name] = v
-        return m
-
-    def exec_module(self, module):
-        m = self._patched_module(module.__name__)
-        assert not isinstance(m.__loader__, PatcherLoader), "Infinite recursion!"
-        m.__loader__.exec_module(m)
-
-    def _patched_module(self, name):
-        patched_fullname = name.replace(self.old_path, self.new_path, 1)
+        patched_fullname = fullname.replace(self.old_path, self.new_path)
         try:
             m = importlib.import_module(patched_fullname)
         except Exception as e:
-            raise type(e)(str(e).replace(self.new_path, self.old_path, 1))
+            raise type(e)(str(e).replace(self.new_path, self.old_path))
+        sys.modules[fullname] = m
+
+        # ensure single copy of sub-modules
+        current_modules = dict(sys.modules)
+        for k, v in current_modules.items():
+            patched_name = k.replace(patched_fullname, fullname, 1)  # arcticdb.blah -> arcticc.blah
+            if patched_name not in current_modules:
+                sys.modules[patched_name] = v
         return m
 
 
-class PatcherFinder(importlib.abc.MetaPathFinder):
-
-    def __init__(self, old_path, new_path, exclusions=None):
-        if exclusions is None:
-            exclusions = []
-        self.patcher_loader = PatcherLoader(old_path=old_path, new_path=new_path)
-        self.old_name = old_path
-        self.new_name = new_path
-        self.exclusions = exclusions
-
-    def find_spec(self, fullname, path, target=None):
-        if self.old_name in fullname and not any(e in fullname for e in self.exclusions):
-            return importlib.util.spec_from_loader(fullname, self.patcher_loader)
+class PatcherFinder:
+    def find_module(self, fullname, path=None):
+        if "arcticc" in fullname and "arcticcxx" not in fullname:
+            return PatcherLoader(old_path="arcticc", new_path="arcticdb")
         return None
 
 
-_old_path = "arcticc"
-_new_path = "arcticdb"
-_exclusions = ("arcticcxx", "arcticdb_ext")
-
-sys.meta_path.append(PatcherFinder(old_path=_old_path, new_path=_new_path, exclusions=_exclusions))
+sys.meta_path.append(PatcherFinder())
