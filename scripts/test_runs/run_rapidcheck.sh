e159dbc09250514976186536c05d9d13208de751
Alex Seaton <alex.seaton@man.com>
2023-05-22 11:09:28 +0100

    Working build tag
    Also clean up some unused scripts

--- scripts/test_runs/run_rapidcheck.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:/opt/man/releases/python-medusa/36-1/lib:/default-pegasus-venv/lib
-
-catchsegv cpp/tests/arcticdb_rapidcheck_tests --gtest_output=xml:gtest-rapidcheck.xml
-
-python3 scripts/test_runs/gtesttojunit.py  "suitename"  "cpprapidcheck"  gtest-rapidcheck.xml  junit-rapidcheck.xml
